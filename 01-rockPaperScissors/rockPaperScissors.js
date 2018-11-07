35eee3ac81a0df306c693bedb6e0e1cfd8cf4303
inceptionJh <kjaehun6546@gmail.com>
2018-11-07 09:08:45 +0900

    temp

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -1,46 +1,34 @@
-/*
-* Write a function that generates every sequence of throws a single
-* player could throw over a three-round game of rock-paper-scissors.
-*
-* Your output should look something like:
-*   [["rock", "rock", "rock"],
-*    ["rock", "rock", "paper"],
-*    ["rock", "rock", "scissors"],
-*    ["rock", "paper", "rock"],
-             ...etc...
-     ]
-*
-* Extra credit:
-*   - Make your function return answers for any number of rounds.
-* Example:
-* rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
-*
-*/
+const choices = ['rock', 'scissors', 'paper']
+const test = []
 
-var rockPaperScissors = function (number) {
-  var makeCases = function(n) {
-    if(n>0) {
-      cases.push()
-      makeCases(--n)
+var makeCases = function (n, arr) {
+
+  if (n !== 0) {
+    for (let choice of choices) {
+      makeCases(--n, arr.concat(choice))
     }
   }
+  else {
+    test.push(arr)    
+  }
 
-  const choices = ['rock', 'scissors', 'paper']
+}
+
+var rockPaperScissors = function (number) {
+
   const cases = []
 
-  if(number === undefined) {
+  if (number === undefined) {
     for (let choice1 of choices) {
-      for(let choice2 of choices) {
-        for(let choice3 of choices) {
+      for (let choice2 of choices) {
+        for (let choice3 of choices) {
           cases.push([choice1, choice2, choice3])
         }
       }
     }
   }
-  else if(typeof number === 'number' && number > 0) {
-    for(let choice of choices) {
-      makeCases(number)
-    }
+  else if (typeof number === 'number' && number > 0) {
+    return makeCases(number, [])
   }
 
   return cases
