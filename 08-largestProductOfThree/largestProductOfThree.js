5876c1a27ad2948e76b9fd7574aea9b68e103b53
inceptionJh <kjaehun6546@gmail.com>
2018-11-16 10:01:25 +0900

    Complete : all

--- 08-largestProductOfThree/largestProductOfThree.js
+++ 08-largestProductOfThree/largestProductOfThree.js
@@ -9,5 +9,12 @@
 
 
 var largestProductOfThree = function(array) {
-  // TODO: everything
-};
+  const allCase = []
+  const arrLen = array.length
+  for (let i = 0; i < arrLen-2; i++)
+    for (let j = i+1; j < arrLen-1; j++)
+      for (let k = j+1; k < arrLen; k++)
+        allCase.push(array[i]*array[j]*array[k])
+  allCase.sort((a,b)=>b-a);
+  return allCase[0];
+}
