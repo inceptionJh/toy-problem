2be00078c8b634b7406c28bcf0cfe8710cfc4a49
inceptionJh <kjaehun6546@gmail.com>
2018-11-27 09:51:37 +0900

    Complete

--- 13-treeBFSelect/treeBFSelect.js
+++ 13-treeBFSelect/treeBFSelect.js
@@ -30,15 +30,26 @@
  * Basic tree that stores a value.
  */
 
-var Tree = function(value){
+var Tree = function (value) {
   this.value = value;
   this.children = [];
+  this.depth = 0;
 };
 
-
-
-Tree.prototype.BFSelect = function(filter) {
+Tree.prototype.BFSelect = function (filter) {
   // return an array of values for which the function filter(value, depth) returns true
+  function pushFiltedChild(children) {
+    for(let child of children) {
+      if(filter(child.value, child.depth)) {
+        selectedChild.push(child.value);
+      }
+      pushFiltedChild(child.children);
+    }
+  }
+
+  const selectedChild = filter(this.value, this.depth) ? [this.value] : [];
+  pushFiltedChild(this.children);
+  return selectedChild;
 };
 
 /**
@@ -49,17 +60,18 @@ Tree.prototype.BFSelect = function(filter) {
   * add an immediate child
   * (wrap values in Tree nodes if they're not already)
   */
-Tree.prototype.addChild = function(child){
-  if (!child || !(child instanceof Tree)){
+Tree.prototype.addChild = function (child) {
+  if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
 
-  if(!this.isDescendant(child)){
+  if (!this.isDescendant(child)) {
     this.children.push(child);
-  }else {
+  } else {
     throw new Error("That child is already a child of this tree");
   }
   // return the new child node for convenience
+  child.depth = this.depth + 1;
   return child;
 };
 
@@ -67,13 +79,13 @@ Tree.prototype.addChild = function(child){
   * check to see if the provided tree is already a child of this
   * tree __or any of its sub trees__
   */
-Tree.prototype.isDescendant = function(child){
-  if(this.children.indexOf(child) !== -1){
+Tree.prototype.isDescendant = function (child) {
+  if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
-  }else{
-    for(var i = 0; i < this.children.length; i++){
-      if(this.children[i].isDescendant(child)){
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].isDescendant(child)) {
         // `child` is descendant of this tree
         return true;
       }
@@ -85,12 +97,12 @@ Tree.prototype.isDescendant = function(child){
 /**
   * remove an immediate child
   */
-Tree.prototype.removeChild = function(child){
+Tree.prototype.removeChild = function (child) {
   var index = this.children.indexOf(child);
-  if(index !== -1){
+  if (index !== -1) {
     // remove the child
-    this.children.splice(index,1);
-  }else{
+    this.children.splice(index, 1);
+  } else {
     throw new Error("That node is not an immediate child of this tree");
   }
 };
