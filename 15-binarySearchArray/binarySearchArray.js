3407407a74b6c0a3c441a7c7627dbb1ae6f9323e
inceptionJh <kjaehun6546@gmail.com>
2018-11-29 10:01:16 +0900

    Not yet

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -8,6 +8,25 @@
  * console.log(index); // [3]
  */
 
-var binarySearch = function (array, target) {
+
+var binarySearch = function (array, target, originalMidIdx=Math.floor(array.length/2)) {
+  let midIdx = Math.floor(array.length / 2);
+  
+  if(array[midIdx] === target) {
+    return originalMidIdx;
+  }
+  
+  if(array[midIdx] < target) {
+    const newArr = array.slice(midIdx + 1, array.length);
+    return binarySearch(newArr, target, originalMidIdx+midIdx);
+  }
+  
+  if(array[midIdx] > target) {
+    const newArr = array.slice(0, midIdx);
+    return binarySearch(newArr, target, originalMidIdx-midIdx);
+  }
 };
 
+const result = binarySearch([1, 2, 3, 4, 5], 2);
+
+console.log(result);
