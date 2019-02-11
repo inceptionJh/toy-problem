37e6d637e1b669748a4a29c87a0295b79fadf42a
inceptionJh <kjaehun6546@gmail.com>
2019-02-11 15:19:57 +0900

    .

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   // --------------STEP 1--------------
   // wrap every word in every `<p>` tag with a `<span>` tag.
   // for example: <p>Hey there</p>
@@ -7,15 +7,18 @@ $(function () {
 
   // TODO: your code here!
   // const pEs = document.querySelectorAll('p'); // pEs : p tag elements
-  // pEs.forEach(el => { 
+  // pEs.forEach(el => {
   //   el.innerHTML =
   //     el.innerText.split(' ').map(str => `<span>${str}</span>`).join('')
   // })
 
-  const $p = $('p');
-  for(let i=0; i<$p.length; i++) {
-    const decoText =
-      $($p[i]).text().split(' ').map(str => `<span>${str}</span>`).join(' ');
+  const $p = $("p");
+  for (let i = 0; i < $p.length; i++) {
+    const decoText = $($p[i])
+      .text()
+      .split(" ")
+      .map((str) => `<span>${str}</span>`)
+      .join(" ");
 
     $($p[i]).html(decoText);
   }
@@ -24,16 +27,16 @@ $(function () {
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
-  const $spans = $('span');
+  const $spans = $("span");
 
   setInterval(() => {
     for (let i = 0; i < $spans.length; i++) {
       const $span = $($spans[i]);
-      const r = Math.random()*255
-      const g = Math.random()*255
-      const b = Math.random()*255
-  
-      $span.css('color', `rgb(${r}, ${g}, ${b})`)
+      const r = Math.random() * 255;
+      const g = Math.random() * 255;
+      const b = Math.random() * 255;
+
+      $span.css("color", `rgb(${r}, ${g}, ${b})`);
     }
   }, 500);
 });
